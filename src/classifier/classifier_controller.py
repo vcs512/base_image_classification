d22ac0204738531bea3f97f538a967a829cbde67
vcs512 <vcsprofissional@gmail.com>
2025-06-12 22:24:23 -0300

    [INFERENCE] GPU support, payload

--- src/classifier/classifier_controller.py
+++ src/classifier/classifier_controller.py
@@ -1,25 +1,76 @@
 import cv2
+import json
+import natsort
+import numpy as np
+import os
 import torch
+from pathlib import Path
+from pprint import pprint
+from typing import List
 
-from .classifier import Classifier
+from src.classifier.classifier import Classifier
+from src.payloads import InferencePayload
+
+
+ENV_VAR_INFERENCE_JSON_PATH = "INFERENCE_JSON_PATH"
+BATCH_SIZE = 64
+
+
+class ClassifierController:
+    """Classification model controller"""
+
+    def __init__(
+        self,
+        model_dir_path: Path,
+        device: torch.device,
+        eval_only_flag: bool
+    ) -> None:
+        self.classifier = Classifier(
+            model_dir_path=model_dir_path,
+            device=device,
+            eval_only_flag=eval_only_flag
+        )
+
+    def get_probabilities(self, input_batch: List[np.ndarray]) -> any:
+        number_mini_batches = (
+            len(input_batch) // BATCH_SIZE + len(input_batch) % BATCH_SIZE
+        )
+        mini_batches = np.array_split(
+            ary=input_batch,
+            indices_or_sections=number_mini_batches
+        )
+        probabilities_list = list()
+        for mini_batch in mini_batches:
+            inference_output = self.classifier.infer(input_batch=mini_batch)
+            probabilities_list.extend(
+                self.classifier.get_probabilities(
+                    inference_output=inference_output
+                )
+            )
+        return probabilities_list
+
 
 if __name__ == "__main__":
-    MODEL_PATH = "./models/swinv2-small-patch4-window8-256"
-    # MODEL_PATH = "./models/vit-small-patch16-224"
-    # MODEL_PATH = "./models/deit-small-patch16-224"
+    inference_json_path = os.environ.get(key=ENV_VAR_INFERENCE_JSON_PATH)
+    with open(file=inference_json_path, mode="r") as fp:
+        inference_json_dict = json.load(fp=fp)
+    inference_payload = InferencePayload(**inference_json_dict)
+    pprint(inference_payload)
 
-    print(f"MODEL_PATH = {MODEL_PATH}")
-    classifier = Classifier(
-        model_dir_path=MODEL_PATH,
+    classifier_controller = ClassifierController(
+        model_dir_path=Path(inference_payload.model_dir_path),
         device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         eval_only_flag=True
     )
 
-    image_path = "./data/sports100/train/air hockey/001.jpg"
-    image = cv2.imread(filename=image_path, flags=cv2.IMREAD_COLOR)
-    image2_path = "./data/sports100/train/volleyball/001.jpg"
-    image2 = cv2.imread(filename=image2_path, flags=cv2.IMREAD_COLOR)
-
-    result = classifier.infer(input_batch=[image, image2])
-    probabilities = classifier.get_probabilities(inference_output=result)
-    print(probabilities)
+    images_path_list = natsort.natsorted(
+        seq=Path(inference_payload.input_images_dir).iterdir()
+    )
+    input_batch = [
+        cv2.imread(filename=filename, flags=cv2.IMREAD_COLOR)
+        for filename in images_path_list
+    ]
+    probabilities = classifier_controller.get_probabilities(
+        input_batch=input_batch
+    )
+    pprint(probabilities)
