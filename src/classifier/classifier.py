67dc57b834b433d88823c87c14c06ed28f3e117c
vcs512 <vcsprofissional@gmail.com>
2025-06-14 11:39:43 -0300

    [INFERENCE] test, docstrings, correct batch

--- src/classifier/classifier.py
+++ src/classifier/classifier.py
@@ -1,8 +1,11 @@
-import torch
 import numpy as np
+import torch
+import transformers.modeling_outputs
 from pathlib import Path
 from transformers import AutoModelForImageClassification, ViTImageProcessor
 from typing import List
+
+from .classifier_interface import ClassificationPipelineOutputInterface
 
 
 class Classifier:
@@ -14,6 +17,13 @@ class Classifier:
         device: torch.device,
         eval_only_flag: bool
     ) -> None:
+        """Construct and initiate model and processor
+
+        Args:
+            model_dir_path (Path): Dir path to compatible model (ViT processor based)
+            device (torch.device): Device to load the model
+            eval_only_flag (bool): If 'True', disable gradient calculus
+        """
         self.device = device
         self.eval_only_flag = eval_only_flag
         self.processor = ViTImageProcessor.from_pretrained(
@@ -28,28 +38,58 @@ class Classifier:
             self._set_model_to_eval()
 
     def _set_model_to_eval(self) -> None:
+        """Disable gradient calculus globally"""
         self.model.eval()
         torch.set_grad_enabled(mode=False)
 
-    def infer(self, input_batch: List[np.ndarray]) -> any:
+    def infer(
+        self,
+        input_batch: List[np.ndarray]
+    ) -> transformers.modeling_outputs.ImageClassifierOutput:
+        """Processor and model forward pass
+
+        Args:
+            input_batch (List[np.ndarray]): List of images to be classified
+                (RGB, (h, w, c))
+
+        Returns:
+            transformers.modeling_outputs.ImageClassifierOutput:
+                Classifier model output (raw)
+        """
         inputs = self.processor(images=input_batch, return_tensors="pt")
         inputs = {k: v.to(self.device) for k, v in inputs.items()}
         outputs = self.model(**inputs)
         return outputs
 
-    def get_probabilities(self, inference_output: any) -> any:
+    def get_probabilities(
+        self,
+        inference_output: transformers.modeling_outputs.ImageClassifierOutput
+    ) -> List[List[ClassificationPipelineOutputInterface]]:
+        """Return formatted probabilities of the classification model
+
+        Args:
+            inference_output (transformers.modeling_outputs.ImageClassifierOutput):
+                Raw inference output of the model
+
+        Returns:
+            List[List[ClassificationPipelineOutputInterface]]: List containing
+                a list of labels and respective scores inferred (sorted from
+                most to least probable)
+        """
         output_logits = inference_output.logits
         batch_probabilities = torch.softmax(input=output_logits, dim=-1)
         batch_probabilities = batch_probabilities.cpu().numpy()
-        class_probabilities_dict_list = list()
+        batch_labels_scores_list = list()
 
         for probabilities in batch_probabilities:
             sorted_idx_list = np.argsort(a=-probabilities)
-            class_probabilities_dict = dict()
+            image_labels_scores_list = list()
             for class_idx in sorted_idx_list:
-                class_name = self.model.config.id2label[class_idx]
-                class_probabilities_dict[class_name] = float(probabilities[class_idx])
-                break
-            class_probabilities_dict_list.append(class_probabilities_dict)
+                output_interface = ClassificationPipelineOutputInterface(
+                    label=self.model.config.id2label[class_idx],
+                    score=float(probabilities[class_idx])
+                )
+                image_labels_scores_list.append(output_interface)
+            batch_labels_scores_list.append(image_labels_scores_list)
 
-        return class_probabilities_dict_list
+        return batch_labels_scores_list
